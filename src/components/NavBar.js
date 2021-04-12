d0dd40a6baa5a6f11f791a571e998233b146ba60
mike <mikesapienzaaudio@gmail.com>
2021-04-12 14:47:27 -0500

    navbar-changes

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -9,19 +9,29 @@ const NavBar = (props) => {
       <Navbar.Brand href="/">Jana Capozzoli</Navbar.Brand>
       <Navbar.Toggle aria-controls="responsive-navbar-nav" />
       <Navbar.Collapse id="responsive-navbar-nav">
-        <Nav navbar variant="pill">
+        <Nav navbar variant="pill" className="ml-auto">
           <Nav.Item>
-            <Nav.Link eventKey="link-1" href="/work_with_me">
-              Work With Me
+            <Nav.Link eventKey="link-1" href="/what_i_do">
+              What I Do
             </Nav.Link>
           </Nav.Item>
           <Nav.Item>
-            <Nav.Link eventKey="link-2" href="/about">
+            <Nav.Link eventKey="link-2" href="/what_i_do">
+              How It Works
+            </Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link eventKey="link-3" href="/what_i_do">
+              Pricing
+            </Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link eventKey="link-4" href="/about">
               About
             </Nav.Link>
           </Nav.Item>
           <Nav.Item>
-            <Nav.Link eventKey="link-3" href="/contact">
+            <Nav.Link eventKey="link-5" href="/contact">
               Contact
             </Nav.Link>
           </Nav.Item>
